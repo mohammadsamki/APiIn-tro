f394efb96148c8d56d605dee72a346e4acde1d42
mohammed alsamaki <86600688+mohammedalsamki@users.noreply.github.com>
2024-02-24 15:31:01 +0300

    added new models

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Employyes, Diabetes, Clothes
 
 # Register your models here.
+
+admin.site.register(Employyes)
+admin.site.register(Diabetes)
+admin.site.register(Clothes)
+
--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+#  emloyyes taple : name, checkin time , checkout time
+
+class Employyes(models.Model):
+    name = models.CharField(max_length=100)
+    checkIn = models.DateTimeField(auto_now_add=True)
+    checkOut = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+
+#  diabetes taple : glocos, bmi, prusser
+
+class Diabetes(models.Model):
+    glocos = models.FloatField(max_length=10)
+    bmi = models.FloatField(max_length=10)
+    prusser = models.FloatField(max_length=10)
+
+    def __str__(self):
+        return self.glocos
+
+#  clothes taple : name, price, description
+
+class Clothes(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField(max_length=10)
+    description = models.TextField()
+    def __str__(self):
+        return self.name
